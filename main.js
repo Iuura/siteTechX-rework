26ccd4311e3c508f33d16523f8f2a3f2c2ce01bb
sipandrei <pontos.serban@gmail.com>
2021-01-08 20:01:48 +0200

    Styling si descriere

---
+++ main.js
@@ -0,0 +1,16 @@
+const extindere = document.querySelectorAll(".extindere");
+const deExtins = document.querySelectorAll(".deExtins");
+
+extindere[0].addEventListener("click", () => extinde(0));
+
+function extinde(x) {
+  if (deExtins[x].classList.contains("ascuns")) {
+    deExtins[x].classList.remove("ascuns");
+    extindere[x].innerHTML =
+      '<img src="/poze/arrow-circle-up.svg" alt="extinde" />';
+  } else {
+    deExtins[x].classList.add("ascuns");
+    extindere[x].innerHTML =
+      '<img src="/poze/arrow-circle-down.svg" alt="extinde" />';
+  }
+}
