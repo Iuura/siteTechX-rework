5496ddde667e91c1d90569f53b5d1b10dafd4320
sipandrei <pontos.serban@gmail.com>
2021-01-09 12:42:30 +0200

    Pagina principala

--- main.js
+++ main.js
@@ -1,16 +1,48 @@
 const extindere = document.querySelectorAll(".extindere");
 const deExtins = document.querySelectorAll(".deExtins");
 
-extindere[0].addEventListener("click", () => extinde(0));
+for (let i = 0; i < extindere.length; i++) {
+   extindere[i].parentElement.addEventListener("click", () => extinde(i));
+}
 
 function extinde(x) {
-  if (deExtins[x].classList.contains("ascuns")) {
-    deExtins[x].classList.remove("ascuns");
-    extindere[x].innerHTML =
-      '<img src="poze/arrow-circle-up.svg" alt="extinde" />';
-  } else {
-    deExtins[x].classList.add("ascuns");
-    extindere[x].innerHTML =
-      '<img src="poze/arrow-circle-down.svg" alt="extinde" />';
-  }
+   let clasa = extindere[x].parentElement.classList[0];
+   switch (clasa) {
+      case "intunecat":
+         if (deExtins[x].classList.contains("ascuns")) {
+            deExtins[x].classList.remove("ascuns");
+            if (deExtins[x].classList.contains("desc")) {
+               deExtins[x].style.display = "flex";
+            }
+            extindere[x].innerHTML =
+               '<img src="poze/arrow-circle-up.svg" alt="extinde" />';
+         } else {
+            deExtins[x].classList.add("ascuns");
+            if (deExtins[x].classList.contains("desc")) {
+               deExtins[x].style.display = "";
+            }
+            extindere[x].innerHTML =
+               '<img src="poze/arrow-circle-down.svg" alt="extinde" />';
+         }
+         break;
+      case "galbui":
+         if (deExtins[x].classList.contains("ascuns")) {
+            deExtins[x].classList.remove("ascuns");
+            if (deExtins[x].classList.contains("desc")) {
+               deExtins[x].style.display = "flex";
+            }
+            extindere[x].innerHTML =
+               '<img src="poze/arrow-circle-up-dark.svg" alt="extinde" />';
+         } else {
+            deExtins[x].classList.add("ascuns");
+            if (deExtins[x].classList.contains("desc")) {
+               deExtins[x].style.display = "";
+            }
+            extindere[x].innerHTML =
+               '<img src="poze/arrow-circle-down-dark.svg" alt="extinde" />';
+         }
+         break;
+   }
+
+   //console.log(extindere[x].parentElement.classList[0]);
 }
