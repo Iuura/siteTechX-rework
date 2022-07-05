b83c550673e2e19283de44d3e55ec47a178f8130
sipandrei <pontos.serban@gmail.com>
2022-07-05 22:15:00 +0300

    performance tweakjs

--- shhh.js
+++ shhh.js
@@ -1,16 +1,21 @@
 const banner = document.querySelector(".banner");
 const hopa = new Audio("./poze/galerie/hopa.mp3");
+const gif = document.querySelector("#surpriza");
 
 let apasari = 0;
 
 hopa.loop = true;
 
 banner.addEventListener("click", () => {
+   let clickuri = 4;
    console.log(apasari);
    apasari++;
-   if (apasari == 8) apasari = 0;
-   if (apasari == 5) hopa.play(0);
-   else if (apasari == 7) {
+   if (apasari == clickuri + 3) apasari = 1;
+   if (apasari == clickuri) {
+      gif.style.display = "block";
+      hopa.play();
+   } else if (apasari == clickuri + 2) {
+      gif.style.display = "none";
       hopa.pause();
       hopa.currentTime = 0;
    }
